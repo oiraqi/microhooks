ef4637e75e3e371ccbf00700aab2f2995f761198
Omar Iraqi <o.iraqi@gmail.com>
2022-01-04 05:58:17 +0100

    updates

--- src/main/java/io/microhooks/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package io.microhooks.util;
-
-public class Constants {
-    private Constants() {}
-    
-    public static final String TRACKED_FIELDS_PREVIOUS_VALUES = "microhooksTrackedFieldsPreviousValues";
-    public static final String TRACKED_FIELDS_PREVIOUS_VALUES_GETTER = "get" + Character.toUpperCase(TRACKED_FIELDS_PREVIOUS_VALUES.charAt(0))
-                                                + TRACKED_FIELDS_PREVIOUS_VALUES.substring(1);
-}
