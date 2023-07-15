af730187d8525b817a29d7ee3e334d5cf51ab5cb
Omar IRAQI <o.iraqi@gmail.com>
2023-07-15 11:25:04 +0100

    updates

--- containers/spring/lib/src/main/java/io/microhooks/containers/spring/SpringApplicationBootstrap.java
+++ containers/spring/lib/src/main/java/io/microhooks/containers/spring/SpringApplicationBootstrap.java
@@ -1,7 +1,5 @@
 package io.microhooks.containers.spring;
 
-import java.lang.reflect.Method;
-
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
