3f4a1017e77917091cac3a1cea02b6b9ad8ccece
Omar IRAQI <o.iraqi@gmail.com>
2023-07-24 10:46:07 +0100

    updates

---
+++ core/lib/src/main/java/io/microhooks/source/EventProducer.java
@@ -0,0 +1,22 @@
+package io.microhooks.source;
+
+import io.microhooks.common.Event;
+import io.microhooks.internal.util.Config;
+
+public class EventProducer {
+
+    private io.microhooks.internal.EventProducer producer;
+
+    public EventProducer() throws Exception {
+        producer = Config.getEventProducer();
+    }
+
+    public void publish(long id, Event<Object> event, String stream) {
+        producer.publish(id, event, stream);
+    }
+
+    public void publish(long id, Event<Object> event, String[] streams) {
+        producer.publish(id, event, streams);
+    }
+    
+}
