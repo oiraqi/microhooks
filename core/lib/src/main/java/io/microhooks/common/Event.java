75f5a0fbf4963d7c5ba3e4149eef075f3f0e7e78
Omar IRAQI <o.iraqi@gmail.com>
2023-07-24 09:53:42 +0100

    updates

--- core/lib/src/main/java/io/microhooks/common/Event.java
+++ core/lib/src/main/java/io/microhooks/common/Event.java
@@ -8,15 +8,16 @@ import lombok.Getter;
 @Getter
 public class Event<P> {
 	public static final String RECORD_CREATED = "_R_C_";
-    public static final String RECORD_UPDATED = "_R_U_";
-    public static final String RECORD_DELETED = "_R_D_";
+	public static final String RECORD_UPDATED = "_R_U_";
+	public static final String RECORD_DELETED = "_R_D_";
 
 	private P payload = null;
 	private String label = null;
 	private long timestamp = 0;
 	private String owner = null;
 
-	public Event() {}
+	public Event() {
+	}
 
 	public Event(P payload, String label, boolean addOwnerToEvent) {
 		this.payload = payload;
